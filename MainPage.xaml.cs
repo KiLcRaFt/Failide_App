a5220283b2ad43ad835897ab7d4d34324bdd8a97
opilane <opilane@HP-CZC2349HV0>
2024-05-07 16:38:30 +0300

    ggwp nichego ne robit

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Internals;
 using System.ComponentModel;
 //using Windows.Foundation.Metadata;
 
@@ -11,11 +12,11 @@ public partial class MainPage : ContentPage
     bool count = true;
     List<Product> products = new List<Product>
         {
-            //new Product {Name= "Name1", Description = "Dis1", Image = "dotnet_Bot.svg"}
+            new Product {Name= "Name1", Description = "Dis1", Image = "dotnet_Bot.svg", Name_ru="Имя1", Description_ru="Дис1"}
         };
     List<Product> products_ru = new List<Product>
         {
-            //new Product {Name= "имя1", Description = "Дис1", Image = "dotnet_Bot.svg"}
+            new Product {Name= "имя1", Description = "Дис1", Image = "dotnet_Bot.svg", Name_ru= "Name1", Description_ru = "Dis1"}
         };
     public MainPage()
 	{
@@ -29,6 +30,7 @@ public partial class MainPage : ContentPage
 
         carouselView.ItemTemplate = new DataTemplate(() =>
 		{
+
             Button btn = new Button
             {
                 Text= "Vahesta",
@@ -53,23 +55,24 @@ public partial class MainPage : ContentPage
             description = new Label { HorizontalTextAlignment = TextAlignment.Center, TextColor = new Color(0, 0, 0), Margin = 10 };
             description.SetBinding(Label.TextProperty, "Description");
 
-            header_ru = new Label
-            {
-                FontAttributes = FontAttributes.Bold,
-                HorizontalTextAlignment = TextAlignment.Center,
-                FontSize = 18,
-                TextColor = new Color(0, 0, 0),
-                Margin = 10,
-                IsVisible= false
-            };
+            //header_ru = new Label
+            //{
+            //    FontAttributes = FontAttributes.Bold,
+            //    HorizontalTextAlignment = TextAlignment.Center,
+            //    FontSize = 18,
+            //    TextColor = new Color(0, 0, 0),
+            //    Margin = 10,
+            //    IsVisible= false
+            //};
 
-            header.SetBinding(Label.TextProperty, "Name_ru");
+            //header_ru.SetBinding(Label.TextProperty, "Name_ru");
 
 
-            description_ru = new Label { HorizontalTextAlignment = TextAlignment.Center, TextColor = new Color(0, 0, 0), Margin = 10, IsVisible = false };
-            description.SetBinding(Label.TextProperty, "Description_ru");
+            //description_ru = new Label { HorizontalTextAlignment = TextAlignment.Center, TextColor = new Color(0, 0, 0), Margin = 10, IsVisible = false };
+            //description_ru.SetBinding(Label.TextProperty, "Description_ru");
 
-            StackLayout st = new StackLayout() { header, header_ru, image, description, description_ru, btn };
+            //StackLayout st = new StackLayout() { header, header_ru, image, description, description_ru, btn };
+            StackLayout st = new StackLayout() { header,image, description, btn };
             st.WidthRequest = 300;
             st.HeightRequest = 300;
             st.BackgroundColor = Colors.WhiteSmoke;
@@ -85,29 +88,81 @@ public partial class MainPage : ContentPage
 
     private void Btn_Clicked(object sender, EventArgs e)
     {
-        
 
         CarouselView carouselView = (CarouselView)Content;
+        int index = carouselView.Position;
+
         if (count)
         {
-            //carouselView.ItemsSource = products;
-            header.IsVisible= false;
-            header_ru.IsVisible= true;
+            foreach (var product in products)
+            {
+                header.Text = product.Name_ru;
+                description.Text = product.Description_ru;
+            }
 
-            description.IsVisible= false;
-            description_ru.IsVisible= true;
             count = !count;
         }
         else
         {
-            //carouselView.ItemsSource = products_ru;
-            header.IsVisible = true;
-            header_ru.IsVisible = false;
+            foreach (var product in products)
+            {
+                header.Text = product.Name;
+                description.Text = product.Description;
+            }
 
-            description.IsVisible = true;
-            description_ru.IsVisible = false;
             count = true;
         }
+
+        //carouselView.Position = index;
+
+        //CarouselView carouselView = (CarouselView)Content;
+        //int index = carouselView.Position;
+        //var product = products[index];
+        //if (count)
+        //{
+        //    //carouselView.ItemsSource = products;
+        //    header.Text = product.Name_ru;
+        //    description.Text = product.Description_ru;
+
+        //    //header.IsVisible = false;
+        //    //header_ru.IsVisible = true;
+        //    //description.IsVisible = false;
+        //    //description_ru.IsVisible = true;
+
+
+        //    //carouselView.Position = index;
+
+        //    count = !count;
+        //}
+        //else
+        //{
+        //    //carouselView.ItemsSource = products;
+
+        //    header.Text = product.Name;
+        //    description.Text = product.Description;
+
+        //    //header.IsVisible = true;
+        //    //header_ru.IsVisible = false;
+        //    //description.IsVisible = true;
+        //    //description_ru.IsVisible = false;
+
+        //    //carouselView.Position = index;
+
+        //    count = true;
+        //}
+
+        ////CarouselView carouselView = (CarouselView)Content;
+
+        ////if (count)
+        ////{
+        ////    carouselView.ItemsSource = products_ru; // Переключаемся на русский список продуктов
+        ////    count = !count;
+        ////}
+        ////else
+        ////{
+        ////    carouselView.ItemsSource = products; // Переключаемся на английский список продуктов
+        ////    count = true;
+        ////}
     }
 
     protected override async void OnAppearing()
@@ -131,7 +186,8 @@ public partial class MainPage : ContentPage
         while (!reader.EndOfStream)
         {
             string line = await reader.ReadLineAsync();
-            string[] parts = line.Split(',');
+            string[] parts = line.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
             if (parts.Length >= 4)
             {
                 products.Add(new Product
@@ -139,18 +195,20 @@ public partial class MainPage : ContentPage
                     Name = parts[0],
                     Description = parts[1],
                     Image = "dotnet_bot.svg",
-                    Name_ru= parts[2],
-                    Description_ru= parts[3],
+                    Name_ru = parts[2],
+                    Description_ru = parts[3],
                 });
-                //products_ru.Add(new Product
-                //{
-                //    Name = parts[2],
-                //    Description = parts[3],
-                //    Image = "dotnet_bot.svg"
-                //});
 
+                products_ru.Add(new Product
+                {
+                    Name = parts[2],
+                    Description = parts[3],
+                    Image = "dotnet_bot.svg",
+                    Name_ru = parts[0],
+                    Description_ru = parts[1],
+                });
+               
             }
-            Console.WriteLine(products+ ", "+products_ru);
         }
     }
 
